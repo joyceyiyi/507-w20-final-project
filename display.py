5e5147f36125445ab3e1472242bf0622782ed6f1
Xiangyu Fan <xyfan@umich.edu>
2020-04-27 16:32:09 -0400

    Finished index page and one viz

--- display.py
+++ display.py
@@ -1,14 +1,51 @@
-import plotly.plotly as py
+from flask import Flask,render_template
 import sqlite3
-import pandas as pd
+import plotly.graph_objs as go
+app = Flask(__name__)
 
-conn = sqlite3.connect('crime.db')
-cursor = conn.cursor()
-query = "SELECT Month, Count(Id) FROM Crimes GROUP BY Month"
-cursor.execute(query)
+@app.route('/')
+@app.template_filter()
+def index():
+	conn = sqlite3.connect('crime.db')
+	cursor = conn.cursor()
+	query = "SELECT SUM(Vandalism),SUM(Assault),SUM(Burglary),SUM(Robbery),SUM(Theft),SUM(Other),SUM(Arrest),SUM(Shooting) FROM DailyStats"
+	rows = cursor.execute(query).fetchall()
+	labels = ['Vandalism','Assault','Burglary','Robbery','Theft','Other','Arrest','Shooting']
+	values = [x for x in rows[0]]
 
-rows = cursor.fetchall()
+	fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+	div = fig.to_html(full_html=False)
 
-df = pd.DataFrame([[ij for ij in i] for i in rows])
-df.rename(columns={0:"Month",1:"Frequency"})
-df = df.sort_values(['Month'],ascending=[1])
+	return render_template('index.html',plot_div=div)
+
+@app.route('/handle_form',methods=["POST"])
+def handle_the_form():
+	start = request.form['start']
+	end = request.form['end']
+	chart = request.form['chart']
+	conn = sqlite3.connect('crime.db')
+	cursor = conn.cursor()
+	query = "SELECT COUNT(Id),Month\
+        FROM (SELECT *\
+                FROM Crimes\
+                WHERE date(Day) BETWEEN '{}' AND '{}') T\
+        GROUP BY MONTH".format(start,end)
+    rows = cursor.execute(query).fetchall()
+    xvals,yvals = [x[1] for x in rows],[x[0] for x in rows]
+    data = []
+    if chart=='line':
+    	data = go.Scatter(x=xvals,y=yvals)
+    if chart = 'bar':
+    	data = go.Bar(x=xvals,y=yvals)
+    basic_layout = go.Layout(title="Cumulative data from {} to {}".format(start,end))
+    fig = go.Figure(data=data,layout=basic_layout)
+    div = fig.to_html(full_html=False)
+	return render_template('graph.html',plot_div=div)
+
+@app.route('/handle_form/detail',methods=["POST"])
+def detail():
+	
+
+if __name__=='__main__':
+	print("starting Flask app",app.name)
+	app.run(debug=True)
